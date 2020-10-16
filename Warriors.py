01bd8848f075f290b7617dba60ccb85c1160d62f
WenYHsieh <48989331+WenYHsieh@users.noreply.github.com>
2020-10-16 11:13:09 +0800

    Template 1016

---
+++ Warriors.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Warriors: 
+    def warriors(self, strength :List[int], attack_range :List[int]):
+        """
+        Given the attributes of each warriors and output the minimal and maximum
+        index of warrior can be attacked by each warrior.
+
+        Parameters:
+          strength (List[int]): The strength value of N warriors
+          attack_range (List[int]): The range value of N warriors
+
+        Returns:
+          attack_interval (List[int]):
+              The min and the max index that the warrior can attack.
+              The format of output is 2N int array `[a0, b0, a1, b1, ...]`
+        """
+        return []
